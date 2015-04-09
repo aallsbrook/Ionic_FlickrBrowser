1649786bd8595bcf59feeb109d95a2f2cfc7d14b
Hanna Lupico <hlupico@gmail.com>
2015-04-09 14:05:28 -0500

    test data

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('myFlickrBrowser.controllers', ['ionic'])
+
+.controller('photoController', function($scope, $http) {
+    $scope.photoList = function() {
+        $http.get("http://echo.jsontest.com/firstname/Hanna/lastname/Lupico", { params: { "key1": "value1", "key2": "value2" } })
+            .success(function(data) {
+                $scope.firstname = data.firstname;
+                $scope.lastname = data.lastname;
+            })
+            .error(function(data) {
+                alert("ERROR");
+            });
+    }
+ 
+});
